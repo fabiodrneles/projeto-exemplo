203a8d3319a4dc87da412ddf48a81ab8c95b74b9
fabiodrneles <fabiodrneles@gmail.com>
2022-05-13 17:15:09 -0300

    adicionando os arquivos readme e pasta assets

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto exemplo
+Atividade guiada
+![Captura de tela de 2022-04-27 01-55-13](https://user-images.githubusercontent.com/42509240/165443718-bd9c08fc-ec72-4cff-bffe-635c7346f982.png)
+
+![Captura de tela de 2022-04-27 01-56-33](https://user-images.githubusercontent.com/42509240/165443735-a5ba42c5-5132-4cfe-a1b8-13c614d7e121.png)
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+let people = [
+    {
+        name: 'Fabio Darci Dorneles',
+        tel: '+55 (55) 9999-9999',
+        xp: true
+    },
+
+    {
+        name: 'Tiffany Beltrão Castanho',
+        tel: '+55 (51) 9999-8888',
+        xp: false 
+    },
+
+    {
+        name: 'Tatiana Azenha Frajuca',
+        tel: '+55 (11) 9999-7777',
+        xp: true
+    },
+
+    {
+        name: 'Luiz Paulo Barbosa',
+        tel: '+55 (14) 9999-6666',
+        xp: false
+    }
+]
+
+for ( person in people ) {
+
+    document.querySelector('table.lista tbody').innerHTML +=
+
+    `<tr>
+    <td>
+       ${people[person].name}
+    </td>
+
+    <td>
+       ${people[person].tel}
+    </td>
+
+    <td>
+       ${(people[person].xp) ? 'Sim' : 'Não'}
+    </td>
+
+    <td>
+       <button> Alterar </button>
+    </td>
+    </tr>`
+
+}
